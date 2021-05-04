53ec7e50745aa85b75e8384ff3bb8e1544f18dfd
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-05-04 15:32:54 +0430

    provider

--- lib/Widgets/RoundedButtonWidget.dart
+++ lib/Widgets/RoundedButtonWidget.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_provider_toplearn/Provider/CounterProvider.dart';
+import 'package:provider/provider.dart';
+
 class RoundedButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final count = Provider.of<CounterProvider>(context);
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         IconButton(
           icon: Icon(Icons.remove),
-          onPressed: () {},
+          onPressed: () {
+            count.removeCount();
+          },
         ),
         SizedBox(
           width: 10,
         ),
-        Text('1'),
+        Text(count.count.toString()),
         SizedBox(
           width: 10,
         ),
         IconButton(
           icon: Icon(Icons.add),
-          onPressed: () {},
+          onPressed: () {
+            count.addCount();
+          },
         ),
       ],
     );
